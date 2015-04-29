f894fa46677f105b677afb702b3fea78c6b0f057
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-04-29 17:11:25 -0400

    Angular

---
+++ pokemon.js
@@ -0,0 +1,7 @@
+// Uses AngularJS - https://github.com/angular/angular.js
+
+var module = angular.module('pokemon');
+
+module.controller('PokemonController', function($scope) {
+	$scope.helloWorld = "hi";
+});
