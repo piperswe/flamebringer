b1ef14256fd0e004c722c02a2cb1daebe62b8362
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-04-29 17:13:03 -0400

    Angular 2

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,6 @@
 // Uses AngularJS - https://github.com/angular/angular.js
 
-var module = angular.module('pokemon');
+var module = angular.module('pokemon', []);
 
 module.controller('PokemonController', function($scope) {
 	$scope.helloWorld = "hi";
