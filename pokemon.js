a39fb2a7b3c9e30d742b7c00d7c85cb5733cb1b7
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-05-06 17:00:02 -0400

    Angular! (pretty much done)

--- pokemon.js
+++ pokemon.js
@@ -11,6 +11,14 @@ module.controller('PokemonController', function($scope) {
 		{
 			url: 'http://ts1.mm.bing.net/th?id=JN.zEV%2bakvqqM%2fCaB67QNVjUg&amp;pid=15.1',
 			title: 'Psychic'
+		},
+		{
+			url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxRjt6E645ROOZftg7cL4gCoox_JPzhCJQTa6Bi1dZMn9ju8720Q',
+			title: 'Fire'
+		},
+		{
+			url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtIscSvUkunDrsQQPNqM80DHPfLcMWbbeGus_4z8sW-2pNMhSjgw"',
+			title: 'Water'
 		}
 	];
 });
