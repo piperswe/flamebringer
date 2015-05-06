5bd810d988ad2f4f79c242001992ecac1fb2a5d5
Zeb McCorkle <19mccorkle.zebulon@brenhamk-12.net>
2015-05-06 16:55:41 -0400

    angular fun

--- pokemon.js
+++ pokemon.js
@@ -3,5 +3,10 @@
 var module = angular.module('pokemon', []);
 
 module.controller('PokemonController', function($scope) {
-	$scope.helloWorld = "hi";
+	$scope.pokemon = [
+		{
+			url: 'http://fc06.deviantart.net/fs70/i/2010/291/0/1/pokemon_type_chart_by_benjamoid-d30zwry.png',
+			title: 'Chart'
+		}
+	];
 });
